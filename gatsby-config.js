a2c47653bc2dfbdd0877717c7622f57dd5edd388
Peter Broadhead <p.t.broadhead@gmail.com>
2022-02-09 09:11:32 +0000

    Latest Update

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,17 +3,26 @@ module.exports = {
       title: `The Singing Mermaid`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
-    }
-  }, "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
-    },
-    __key: "images"
-  }]
+  plugins: [
+    "gatsby-plugin-image", 
+    "gatsby-plugin-react-helmet", 
+    "gatsby-plugin-fontawesome-css",
+    "gatsby-plugin-sitemap",
+    "gatsby-plugin-sharp", 
+    "gatsby-transformer-sharp", 
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        "icon": "src/images/the-singing-mermaid-icon.png"
+        }
+    }, 
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        "name": "images",
+        "path": "./src/images/"
+      },
+      __key: "images"
+    }    
+  ]
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,182 +1,114 @@
 import * as React from "react"
+import Mermaid from "../images/the singing mermaid with stars.png"
+import "../components/index.css"
+import { Helmet } from 'react-helmet'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faStar } from '@fortawesome/free-solid-svg-icons'
 
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-const headingAccentStyles = {
-  color: "#663399",
-}
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-}
-const listItemStyles = {
-  fontWeight: 300,
-  fontSize: 24,
-  maxWidth: 560,
-  marginBottom: 30,
-}
 
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: 16,
-  verticalAlign: "5%",
-}
 
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  marginBottom: 24,
-}
-
-const descriptionStyle = {
-  color: "#232129",
-  fontSize: 14,
-  marginTop: 10,
-  marginBottom: 0,
-  lineHeight: 1.25,
-}
-
-const docLink = {
-  text: "Documentation",
-  url: "https://www.gatsbyjs.com/docs/",
-  color: "#8954A8",
-}
-
-const badgeStyle = {
-  color: "#fff",
-  backgroundColor: "#088413",
-  border: "1px solid #088413",
-  fontSize: 11,
-  fontWeight: "bold",
-  letterSpacing: 1,
-  borderRadius: 4,
-  padding: "4px 6px",
-  display: "inline-block",
-  position: "relative",
-  top: -2,
-  marginLeft: 10,
-  lineHeight: 1,
-}
-
-// data
-const links = [
+const reviews = [
   {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial/",
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-    color: "#E95800",
+    source: "Time Out",
+    stars: "four",
+    url: "",
+    excerptOne:
+      "‘It’s a lovely piece of theatre that’ll leave its audience in a trance, while possessing enough loopy visual brio to ensure adults have a swimming time too.’",
+    excerptTwo:
+      "‘brings the world illustrated by Lydia Monks to gloriously barmy life.’",
   },
   {
-    text: "How to Guides",
-    url: "https://www.gatsbyjs.com/docs/how-to/",
-    description:
-      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-    color: "#1099A8",
+    source: "The Times",
+    stars: "four",
+    url: "",
+    excerptOne:
+      "‘Here’s a lovely little show that makes a big splash. it thrills and charms, effortlessly transporting its audience from the depths of the ocean to dizzying high-wire feats in a circus big top’",
+    excerptTwo:
+      "‘Exquisitely designed puppets manipulated with captivating skill transform Julia Donaldson’s book into an entrancing stage show’",
   },
   {
-    text: "Reference Guides",
-    url: "https://www.gatsbyjs.com/docs/reference/",
-    description:
-      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-    color: "#BC027F",
+    source: "The Guardian",
+    stars: "none",
+    url: "",
+    excerptOne:
+      "‘Bad puns, circus tricks and goggle-eyed jellyfish abound in this smart adaptation of the popular picture book’",
+    excerptTwo:
+      "‘At the end, 4 year old Hilda is still laughing about an opening gag about how to do a handstand. This is a fun, warm-hearted adventure that may leave you humming its songs – and even wanting to hug a seagull. ",
   },
   {
-    text: "Conceptual Guides",
-    url: "https://www.gatsbyjs.com/docs/conceptual/",
-    description:
-      "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
-    color: "#0D96F2",
+    source: "Musical Theatre Review",
+    stars: "four",
+    url: "",
+    excerptOne:
+      "‘As entertainment for very young children, musical shows don’t get much better than this.’",
   },
   {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-    description:
-      "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-    color: "#8EB814",
-  },
-  {
-    text: "Build and Host",
-    url: "https://www.gatsbyjs.com/cloud",
-    badge: true,
-    description:
-      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
-    color: "#663399",
+    source: "Edinburgh Tourist",
+    stars: "five",
+    url: "",
+    excerptOne:
+      "‘Kids and grown ups alike had a great time…It ran at 55 minutes which was just right. Absolutely recommended for families with young children.’",
   },
 ]
-
-// markup
 const IndexPage = () => {
-  return (
-    <main style={pageStyles}>
-      <title>Home Page</title>
-      <h1 style={headingStyles}>
-        Happy Days
-        <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! </span>
-        <span role="img" aria-label="Party popper emojis">
-          🎉🎉🎉
-        </span>
+  return (    
+    <main>
+      <Helmet title="The Singing Mermaid" defer={false} />            
+      <h1>
+        <img src={Mermaid} alt='The Singing Mermaid'></img>
       </h1>
-      <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.js</code> to see this page
-        update in real-time.{" "}
-        <span role="img" aria-label="Sunglasses smiley emoji">
-          😎
-        </span>
+      <p class={"big-quote"}>
+        Did you ever go to Silversands
+        <br></br>
+        On a sunny summer’s day?
+        <br></br>
+        Then perhaps you saw the mermaid
+        <br></br>
+        Who sang in the deep blue bay
       </p>
-      <ul style={listStyles}>
-        <li style={docLinkStyle}>
-          <a
-            style={linkStyle}
-            href={`${docLink.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-          >
-            {docLink.text}
-          </a>
-        </li>
-        {links.map(link => (
-          <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
-            <span>
-              <a
-                style={linkStyle}
-                href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-              >
-                {link.text}
-              </a>
-              {link.badge && (
-                <span style={badgeStyle} aria-label="New Badge">
-                  NEW!
-                </span>
-              )}
-              <p style={descriptionStyle}>{link.description}</p>
+      <p>
+        One day the singing mermaid is tempted away from all of her sea-creature friends to join a travelling circus. The audiences love to hear her sing but the poor mermaid was tricked! Instead of the swimming pool she was promised, she is kept in a small fish tank by the wicked circus ringmaster, Sam Sly. She is very unhappy, and longs to go back to Silversands bay, but will she ever escape? Who is there to help her?
+      </p>
+      <div class={"show-info"}>        
+        <p>  
+          With beautiful puppetry and performance, and music and songs by Barb Jungr, Watershed Productions are proud to present this Little Angel Theatre and Royal and Derngate production. 
+        </p>
+        <p> 
+          Age recommendation 3-8 yrs. 
+        </p>
+        <p> 
+          Duration 55 minutes. No interval.
+        </p>
+        <a href={"https://polkatheatre.com/event/the-singing-mermaid/"}>
+          Book Now at the Polka Theatre
+        </a>
+        <a href={"https://www.littleangeltheatre.com/whats-on/the-singing-mermaid/"}>
+          Book Now at the Little Angel Theatre
+        </a>
+      </div>
+      <ul>
+        {reviews.map(review => (
+          <li key={review.url}>
+            <span class={review.stars}>
+              <FontAwesomeIcon icon={faStar} size="1x" />
+              <FontAwesomeIcon icon={faStar} size="1x" />
+              <FontAwesomeIcon icon={faStar} size="1x" />
+              <FontAwesomeIcon icon={faStar} size="1x" />
+              <FontAwesomeIcon icon={faStar} size="1x" />
+              <a href={`${review.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}>
+                {review.source}
+              </a>              
+              <p>{review.excerptOne}</p>
+              <p>{review.excerptTwo}</p>
             </span>
           </li>
         ))}
       </ul>
-      <img
-        alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
-      />
+      <article>
+        <ul>
+
+        </ul>
+      </article>
     </main>
   )
 }
