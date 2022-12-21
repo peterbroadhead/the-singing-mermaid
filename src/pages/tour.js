3cac4b6a40d17238a3707b0e9b57594a1332da62
Peter Broadhead <p.t.broadhead@gmail.com>
2022-12-21 21:47:43 +0000

    Updated tour

--- src/pages/tour.js
+++ src/pages/tour.js
@@ -8,47 +8,56 @@ const SongsPage = () => {
     <main>
       <Helmet title="The Singing Mermaid - Book Now" defer={false} description="A play for children based on the orginal book The Singing Mermaid written by Julia Donaldson and illustrated by Lydia Monks."/>            
       <Menu />
-      <h1>Book your ticket for The Singing Mermaid</h1>  
-      <ul>
+      <h1>Book your ticket for The Singing Mermaid</h1>              
+      <ul className="shows">
         <li>
           <a href={"https://www.lakesidearts.org.uk/"} target="_blank" rel="noreferrer">
-          Nottingham - Lakeside Arts <br></br>9 to 31 December 2022 <br></br>Tickets selling fast!
+          <h3>Nottingham</h3>Lakeside Arts <br></br><span>9 to 31 December 2022</span><br></br>Tickets selling fast!
           </a>
         </li>
         <li>
           <a href={"https://www.artsdepot.co.uk/"} target="_blank" rel="noreferrer">
-          London N12 - artsdepot <br></br>12 to 14 February 2023 <br></br>Tickets now on sale!
-          </a>
-        </li> 
-        <li>
-          <a href={"https://watersidearts.org/whats-on/3221-the-singing-mermaid/"} target="_blank" rel="noreferrer">
-          Manchester - Waterside Arts <br></br>17 to 19 February 2023 <br></br>Tickets on sale now!
+          <h3>London N12</h3>artsdepot<br></br><span>12 to 14 February</span>
           </a>
         </li> 
         <li>
           <a href={"https://northernstage.co.uk/"} target="_blank" rel="noreferrer">
-          Newcastle on Tyne - Northern Stage<br></br>21 to 23 Feb 2023 <br></br>Tickets on sale now!
+          <h3>Newcastle on Tyne</h3> Northern Stage<br></br><span>21 to 23 Feb</span>
           </a>
         </li> 
         <li>
           <a href={"https://www.polkatheatre.com"} target="_blank" rel="noreferrer">
-          London SW19 - Polka Theatre<br></br>1 to 8 April 2023 <br></br>On sale now!
+          <h3>London SW19</h3>Polka Theatre<br></br><span>1 to 8 April</span>
           </a>
         </li> 
         <li>
           <a href={"https://www.anvilarts.org.uk"} target="_blank" rel="noreferrer">
-          Basingstoke - Haymarket Theatre<br></br>12 & 13 April 2023 <br></br>Tickets on sale soon!
+          <h3>Basingstoke</h3>Haymarket Theatre<br></br><span>12 & 13 April</span>
           </a>
         </li> 
-
         <li>
           <a href={"https://www.theatrclwyd.com/"} target="_blank" rel="noreferrer">
-          Mold - Theatr Clwyd<br></br>15 & 16 April 2023 <br></br>On sale now!
+          <h3>Mold</h3>Theatr Clwyd<br></br><span>15 & 16 April</span>
           </a>
         </li> 
         <li>
+          <a href={"https://www.birmingham-rep.co.uk/"} target="_blank" rel="noreferrer">
+          <h3>Birmingham</h3> Birmingham Rep<br></br><span>27 & 28 May</span>
+          </a>
+        </li>  
+        <li>
+          <a href={"https://dukeslancaster.org/"} target="_blank" rel="noreferrer">
+          <h3>Lancaster</h3>The Dukes<br></br><span>30 & 31 May</span>
+          </a>
+        </li>   
+        <li>
           <a href={"https://www.theatreroyal.org"} target="_blank" rel="noreferrer">
-          Bury St Edmunds – Theatre Royal<br></br>2 & 3 June 2023 <br></br>On sale now! 
+          <h3>Bury St Edmunds</h3>Theatre Royal<br></br><span>2 & 3 June</span> 
+          </a>
+        </li>   
+        <li>
+          <a href={"https://www.theatreroyal.org"} target="_blank" rel="noreferrer">
+          <h3>Manchester</h3>HOME<br></br><span>24 to 26 Oct</span>
           </a>
         </li>                                               
         <li>Coming to a Theatre near you soon...</li>
