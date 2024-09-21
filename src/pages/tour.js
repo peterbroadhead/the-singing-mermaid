96f4810b697eedf999d9530c073515e993803fed
Peter Broadhead <p.t.broadhead@gmail.com>
2024-09-21 10:04:00 +0100

    Update tour.js

--- src/pages/tour.js
+++ src/pages/tour.js
@@ -15,23 +15,8 @@ const SongsPage = () => {
       <h3>If you’re coming to a Relaxed Performance or would like to know more about the story and the production, <a href={visualStory} target="_blank" rel="noreferrer"> click here.</a></h3>
       <ul className="shows">
         <li>
-          <a href={"https://www.artsdepot.co.uk/event/the-singing-mermaid/"} target="_blank" rel="noreferrer">
-          <h3>London N12</h3>artsdepot<br></br><span>30 March to 1 April</span>
-          </a>
-        </li> 
-        <li>
-          <a href={"https://greenwichtheatre.org.uk/"} target="_blank" rel="noreferrer">
-          <h3>London SE10</h3>Greenwich Theatre<br></br><span>3 & 4 April</span>
-          </a>
-        </li>
-        <li>
-          <a href={"https://thegulbenkian.co.uk/events/the-singing-mermaid/"} target="_blank" rel="noreferrer">
-          <h3>Canterbury</h3>The Gulbenkian<br></br><span>9 to 10 April</span>
-          </a>
-        </li>
-        <li>
-          <a href={"https://www.theatreroyalwinchester.co.uk/whats-on/singing-mermaid"} target="_blank" rel="noreferrer">
-          <h3>Winchester</h3>Theatre Royal<br></br><span>13 to 14 April</span>
+          <a href={"https://www.littleangeltheatre.com/whats-on/the-singing-mermaid-2024/"} target="_blank" rel="noreferrer">
+          <h3>London N1 2DN</h3>Little Angel Theatre<br></br><span>23 Nov–26 Jan 2025</span>
           </a>
         </li> 
       </ul>
