931e227d7d8a41476a8f16420a2cbae5dcab203b
Peter Broadhead <p.t.broadhead@gmail.com>
2025-06-17 19:40:29 +0100

    2025 Shows

--- src/pages/tour.js
+++ src/pages/tour.js
@@ -15,8 +15,13 @@ const SongsPage = () => {
       <h3>If you’re coming to a Relaxed Performance or would like to know more about the story and the production, <a href={visualStory} target="_blank" rel="noreferrer"> click here.</a></h3>
       <ul className="shows">
         <li>
-          <a href={"https://www.littleangeltheatre.com/whats-on/the-singing-mermaid-2024/"} target="_blank" rel="noreferrer">
-          <h3>London N1 2DN</h3>Little Angel Theatre<br></br><span>23 Nov–26 Jan 2025</span>
+          <a href={"https://www.yvonne-arnaud.co.uk/whats-on/the-singing-mermaid"} target="_blank" rel="noreferrer">
+          <h3>Millbrook, Guildford GU1 3UX</h3> Yvonne Arnaud Theatre<br></br><span>20 Aug - Sun 24 Aug 2025</span>
+          </a>
+        </li> 
+        <li>
+          <a href={"https://queens-theatre.co.uk/whatson/the-singing-mermaid/"} target="_blank" rel="noreferrer">
+          <h3>Queen's Theatre Hornchurch</h3>Little Angel Theatre<br></br><span>28 - 30 Aug 2025</span>
           </a>
         </li> 
       </ul>
