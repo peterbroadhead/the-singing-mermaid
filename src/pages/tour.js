f63db490a5bb51d063932cfae495b750b29ff741
Peter Broadhead <p.t.broadhead@gmail.com>
2025-12-03 20:54:26 +0000

    2026 Tour Dates

--- src/pages/tour.js
+++ src/pages/tour.js
@@ -14,16 +14,31 @@ const SongsPage = () => {
       <h1>Book your ticket for The Singing Mermaid</h1>
       <h3>If you’re coming to a Relaxed Performance or would like to know more about the story and the production, <a href={visualStory} target="_blank" rel="noreferrer"> click here.</a></h3>
       <ul className="shows">
-        <li>
-          <a href={"https://www.yvonne-arnaud.co.uk/whats-on/the-singing-mermaid"} target="_blank" rel="noreferrer">
-          <h3>Millbrook, Guildford GU1 3UX</h3> Yvonne Arnaud Theatre<br></br><span>20 Aug - Sun 24 Aug 2025</span>
+         <li>
+          <a href={"https://artsdepot.co.uk/event/the-singing-mermaid/"} target="_blank" rel="noreferrer">
+          <h3>North Finchley</h3> artsdepot<br></br><span>Mon 16 - Tues 17 Feb 2026</span>
           </a>
         </li> 
         <li>
-          <a href={"https://queens-theatre.co.uk/whatson/the-singing-mermaid/"} target="_blank" rel="noreferrer">
-          <h3>Queen's Theatre Hornchurch</h3>Little Angel Theatre<br></br><span>28 - 30 Aug 2025</span>
+          <a href={"https://redgravetheatre.com/event/2026/02/the-singing-mermaid/2275/"} target="_blank" rel="noreferrer">
+          <h3>Bristol</h3> Redgrave Theatre<br></br><span>Sat 21 - Sun 22 Feb 2026</span>
           </a>
         </li> 
+        <li>
+          <a href={"https://redgravetheatre.com/event/2026/02/the-singing-mermaid/2275/"} target="_blank" rel="noreferrer">
+          <h3>Manchester</h3> Home<br></br><span>Sat 23 - Sun 24 May 2026</span>
+          </a>
+        </li> 
+        <li>
+          <a href={"https://redgravetheatre.com/event/2026/02/the-singing-mermaid/2275/"} target="_blank" rel="noreferrer">
+          <h3>Durham</h3> Gala Theatre<br></br><span>Wed 27 Thu - 28 May 2026</span>
+          </a>
+        </li> 
+        <li>
+          <a href={"https://thegulbenkian.co.uk/events/the-singing-mermaid-2/"} target="_blank" rel="noreferrer">
+          <h3>Canterbury</h3> Gulbenkian Theatre<br></br><span>Sat 30 - Sun 31 May</span>
+          </a>
+        </li>   
       </ul>
     </main>
   )
