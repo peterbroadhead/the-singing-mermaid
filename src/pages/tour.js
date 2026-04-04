63daf18cc3e63cd8c1979f46f2683d332b560023
Peter Broadhead <p.t.broadhead@gmail.com>
2026-04-04 11:25:59 +0100

    Update tour.js

--- src/pages/tour.js
+++ src/pages/tour.js
@@ -14,16 +14,6 @@ const SongsPage = () => {
       <h1>Book your ticket for The Singing Mermaid</h1>
       <h3>If you’re coming to a Relaxed Performance or would like to know more about the story and the production, <a href={visualStory} target="_blank" rel="noreferrer"> click here.</a></h3>
       <ul className="shows">
-         <li>
-          <a href={"https://artsdepot.co.uk/event/the-singing-mermaid/"} target="_blank" rel="noreferrer">
-          <h3>North Finchley</h3> artsdepot<br></br><span>Mon 16 - Tues 17 Feb 2026</span>
-          </a>
-        </li> 
-        <li>
-          <a href={"https://redgravetheatre.com/event/2026/02/the-singing-mermaid/2275/"} target="_blank" rel="noreferrer">
-          <h3>Bristol</h3> Redgrave Theatre<br></br><span>Sat 21 - Sun 22 Feb 2026</span>
-          </a>
-        </li> 
         <li>
           <a href={"https://homemcr.org/whats-on/the-singing-mermaid-yyxl"} target="_blank" rel="noreferrer">
           <h3>Manchester</h3> Home<br></br><span>Sat 23 - Sun 24 May 2026</span>
